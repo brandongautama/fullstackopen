355fab410a1045bf5d93ff20458ab8b470f5ec07
Brandon Gautama <bgautama@amazon.com>
2024-04-23 11:46:51 -0700

    exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,11 +2,21 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
       {props.part} {props.exercises}
     </p>
+  );
+};
+
+const Content = ({ exercises }) => {
+  return (
+    <div>
+      <Part part={exercises.part1} exercises={exercises.exercises1} />
+      <Part part={exercises.part2} exercises={exercises.exercises2} />
+      <Part part={exercises.part3} exercises={exercises.exercises3} />
+    </div>
   );
 };
 
@@ -23,12 +33,19 @@ const App = () => {
   const part3 = "State of a component";
   const exercises3 = 14;
 
+  const exercises = {
+    part1,
+    exercises1,
+    part2,
+    exercises2,
+    part3,
+    exercises3,
+  };
+
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content exercises={exercises} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );

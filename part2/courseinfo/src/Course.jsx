cd4df5e66b3f3520fffc25642ef36b66a614b799
Brandon Gautama <bgautama@amazon.com>
2024-04-24 20:21:10 -0700

    exercise 2.5

---
+++ part2/courseinfo/src/Course.jsx
@@ -0,0 +1,44 @@
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ course }) => {
+  return (
+    <p>
+      <strong>
+        total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)}{' '}
+        exercises
+      </strong>
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </>
+  );
+};
+
+export default Course;

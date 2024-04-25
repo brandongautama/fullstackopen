c49edbeb8eeb5967d245bffce8586289ae62418b
Brandon Gautama <bgautama@amazon.com>
2024-04-24 23:24:15 -0700

    exercise 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -33,9 +33,13 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons([...persons, { name: newName, number: newNumber }]);
-    setNewName('');
-    setNewNumber('');
+    const newPerson = { name: newName, number: newNumber };
+
+    axios.post('http://localhost:3001/persons', newPerson).then((response) => {
+      setPersons(persons.concat(response.data));
+      setNewName('');
+      setNewNumber('');
+    });
   };
 
   const handleSearchFilter = (event) => {

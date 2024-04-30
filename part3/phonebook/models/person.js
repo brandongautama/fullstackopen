8309c007cacd6c4a43ca7d23d32d42b330677405
Brandon Gautama <bgautama@amazon.com>
2024-04-29 19:29:27 -0700

    exercise 3.20

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -20,7 +20,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /\d{3}\-\d+$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number`,
+    },
+  },
 });
 
 personSchema.set('toJSON', {

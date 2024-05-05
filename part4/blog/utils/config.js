aceeed97166ffb9192293df4e59f5b7605ccf0c6
Brandon Gautama <brandongautama15@gmail.com>
2024-05-05 16:11:14 -0700

    exercise 4.2

---
+++ part4/blog/utils/config.js
@@ -0,0 +1,8 @@
+const PORT = 3003;
+const MONGODB_URI =
+  'mongodb+srv://fullstackopen:fullstackopen@cluster0.d0ctkrw.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0';
+
+module.exports = {
+  PORT,
+  MONGODB_URI,
+};

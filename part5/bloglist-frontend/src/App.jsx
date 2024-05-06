3c76c6e138848e4201a7696678225a4e4956fe02
Brandon Gautama <brandongautama15@gmail.com>
2024-05-06 16:59:49 -0700

    exercise 5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -22,10 +22,20 @@ const App = () => {
     blogService.getAll().then(blogs => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedInUser = window.localStorage.getItem('user');
+    if (loggedInUser) {
+      console.log(`User found ${loggedInUser}`);
+      const user = JSON.parse(loggedInUser);
+      setUser(user);
+    }
+  }, []);
+
   const handleLogin = async event => {
     event.preventDefault();
     try {
       const user = await loginService.login({ username, password });
+      window.localStorage.setItem('user', JSON.stringify(user));
       console.log(user);
       setUser(user);
       setUsername('');
@@ -39,8 +49,14 @@ const App = () => {
     console.log('logging in with', username, password);
   };
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('user');
+    setUser(null);
+  };
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
+      <h2>Log in to application</h2>
       <div>
         username{' '}
         <input
@@ -63,11 +79,18 @@ const App = () => {
     </form>
   );
 
+  const logoutButton = () => (
+    <div>
+      <span>{user.name} logged-in</span>
+      <button onClick={handleLogout}>logout</button>
+    </div>
+  );
+
   return (
     <div>
       <h2>blogs</h2>
       <Notification message={errorMessage} />
-      {user === null ? loginForm() : <p>{user.name} logged-in</p>}
+      {user === null ? loginForm() : logoutButton()}
       {user !== null && blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
     </div>
   );

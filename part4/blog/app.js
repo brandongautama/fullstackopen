eb8a7acd4e7aaae05f4a23ab2efa7d0484dbe762
Brandon Gautama <brandongautama15@gmail.com>
2024-05-05 23:29:38 -0700

    exercise 4.15

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -6,6 +6,7 @@ require('express-async-errors');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 
 mongoose.connect(config.MONGODB_URI);
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);

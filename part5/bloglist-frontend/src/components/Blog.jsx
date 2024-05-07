d437f1d8ac8118558a5f7a2138cbb00152f3f8e7
Brandon Gautama <brandongautama15@gmail.com>
2024-05-06 21:15:44 -0700

    exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div className='blog'>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  return (
+    <div className='blog'>
+      <p>
+        {blog.title} {blog.author}{' '}
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {' '}
+          {showDetails ? 'hide' : 'view'}{' '}
+        </button>
+      </p>
+      {showDetails && (
+        <>
+          <p>{blog.url}</p>
+          <p>{blog.likes}</p>
+          <p>{blog.user && blog.user.name}</p>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Blog;

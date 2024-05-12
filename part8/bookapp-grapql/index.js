e6139c46fe091e5d9494fefc9cf2121bafdd73d3
Brandon Gautama <brandongautama15@gmail.com>
2024-05-12 15:20:18 -0700

    exercise 8.3

--- part8/bookapp-grapql/index.js
+++ part8/bookapp-grapql/index.js
@@ -98,9 +98,26 @@ let books = [
 */
 
 const typeDefs = `
+  type Book {
+    title: String!
+    published: Int
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -108,6 +125,14 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
+    allAuthors: () => authors,
+  },
+
+  Author: {
+    bookCount: root => {
+      return books.filter(book => book.author === root.name).length;
+    },
   },
 };
 

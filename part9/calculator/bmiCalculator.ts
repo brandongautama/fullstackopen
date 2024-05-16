f9eab1566c4ce488cbd188679a261c40b614bf5e
Brandon Gautama <brandongautama15@gmail.com>
2024-05-15 22:13:20 -0700

    exercise 9.3

--- part9/calculator/bmiCalculator.ts
+++ part9/calculator/bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface Arguments {
+    height: number;
+    weight: number;
+}
+
+const parseArguments = (args: string[]): Arguments => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not numbers!')
+    }
+}
+
 const calculateBmi = (height: number, weight: number): string => {
     const bmi: number = weight*10000/(height*height)
     switch (true) {
@@ -13,4 +32,13 @@ const calculateBmi = (height: number, weight: number): string => {
 }
 
 
-console.log(calculateBmi(180, 74));
+try {
+    const { height, weight} = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened';
+    if (error instanceof Error) {
+        errorMessage += error.message;
+    }
+    console.log(errorMessage)
+}
---
+++ part9/calculator/exerciseCalculator.ts
@@ -0,0 +1,57 @@
+interface Arguments2 {
+    dailyExerciseHours: number[];
+    target: number
+}
+
+const parseArguments2 = (args: string[]): Arguments2 => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    const [first, second, target, ...dailyExerciseHours] = args;
+
+
+
+    if (!isNaN(Number(target)) && dailyExerciseHours.map(h => !isNaN(Number(h)))) {
+        return {
+            dailyExerciseHours: dailyExerciseHours.map(h => Number(h)),
+            target: Number(target)
+        }
+    } else {
+        throw new Error('Provided values were not numbers!')
+    }
+}
+
+interface Result {
+    periodLength: number,
+    trainingDays: number,
+    target: number,
+    average: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string
+}
+
+const calculateExercises = (dailyExerciseHours: number[], target: number): Result => {
+    const average: number = dailyExerciseHours.reduce((sum, curr) => sum + curr, 0) / dailyExerciseHours.length
+    const rating: number = (average - target) > 0 ? 3 : ((average - target) > -1 ? 2 : 1)
+    return {
+        periodLength: dailyExerciseHours.length,
+        trainingDays: dailyExerciseHours.filter(h => h !== 0).length,
+        target: target,
+        average: average,
+        success: average >= target,
+        rating: rating,
+        ratingDescription: rating === 3 ? 'great' : (rating === 2 ? 'not too bad but could be better' : 'try harder')
+    }
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+
+try {
+    const {dailyExerciseHours, target} = parseArguments2(process.argv)
+    console.log(calculateExercises(dailyExerciseHours, target));
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened';
+    if (error instanceof Error) {
+        errorMessage += error.message;
+    }
+    console.log(errorMessage)
+}

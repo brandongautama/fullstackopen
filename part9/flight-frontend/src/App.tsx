9d531829db22d68bf0f8b71633850d54d7ddc297
Brandon Gautama <brandongautama15@gmail.com>
2024-05-16 20:59:13 -0700

    exercise 9.19

--- part9/flight-frontend/src/App.tsx
+++ part9/flight-frontend/src/App.tsx
@@ -1,35 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import { DiaryEntry } from '../types';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [newDate, setNewDate] = useState('');
+  const [newWeather, setNewWeather] = useState('');
+  const [newVisibility, setNewVisibility] = useState('');
+  const [newComment, setNewComment] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    axios
+      .get<DiaryEntry[]>('http://localhost:3000/api/diaries')
+      .then(response => {
+        setDiaries(response.data);
+      });
+  }, []);
+
+  const submitForm = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const newDiary = {
+      date: newDate,
+      weather: newWeather,
+      comment: newComment,
+      visibility: newVisibility,
+    };
+    try {
+      const response = await axios.post<DiaryEntry>(
+        'http://localhost:3000/api/diaries',
+        newDiary
+      );
+      setDiaries([...diaries, response.data]);
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error) && error.response) {
+        setErrorMessage(error.response.data);
+        setTimeout(() => setErrorMessage(''), 5000);
+      }
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {errorMessage}
+      <form onSubmit={submitForm}>
+        <div>
+          date
+          <input
+            type='date'
+            value={newDate}
+            onChange={event => setNewDate(event.target.value)}
+          />
+        </div>
+        <div>
+          weather sunny
+          <input
+            type='radio'
+            value={newWeather}
+            onChange={() => setNewWeather('sunny')}
+          />
+          rainy
+          <input
+            type='radio'
+            value={newWeather}
+            onChange={() => setNewWeather('rainy')}
+          />
+          cloudy
+          <input
+            type='radio'
+            value={newWeather}
+            onChange={() => setNewWeather('cloudy')}
+          />
+          stormy
+          <input
+            type='radio'
+            value={newWeather}
+            onChange={() => setNewWeather('stormy')}
+          />
+          windy
+          <input
+            type='radio'
+            value={newWeather}
+            onChange={() => setNewWeather('windy')}
+          />
+        </div>
+        <div>
+          comment
+          <input
+            value={newComment}
+            onChange={event => setNewComment(event.target.value)}
+          />
+        </div>
+        <div>
+          visibility great{' '}
+          <input
+            type='radio'
+            value={newVisibility}
+            onChange={() => setNewVisibility('great')}
+          />
+          good{' '}
+          <input
+            type='radio'
+            value={newVisibility}
+            onChange={() => setNewVisibility('good')}
+          />
+          ok{' '}
+          <input
+            type='radio'
+            value={newVisibility}
+            onChange={() => setNewVisibility('ok')}
+          />
+          poor{' '}
+          <input
+            type='radio'
+            value={newVisibility}
+            onChange={() => setNewVisibility('poor')}
+          />
+        </div>
+        <button type='submit'>add</button>
+      </form>
+      <ul>
+        {diaries.map(diary => (
+          <li key={diary.id}>
+            <strong>{diary.date}</strong> {diary.weather} {diary.visibility}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
